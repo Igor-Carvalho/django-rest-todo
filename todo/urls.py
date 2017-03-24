98c7b0023a76770616a825f58ad5226da475a21f
Igor Carvalho <igor.dev.py@gmail.com>
2017-03-24 19:40:45 -0300

    Adicionado esquema e documentação da API

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf import settings, urls
 from django.contrib import admin
-from rest_framework import routers
+from rest_framework import documentation, routers, schemas
 from ttasks.views import TaskAPI, tasks
 
 router = routers.DefaultRouter()
@@ -12,6 +12,8 @@ urlpatterns = [
     urls.url(r'^$', tasks, name='tasks'),
     urls.url(r'^api/v1/', urls.include(router.urls, namespace='v1')),
     urls.url(r'^rest_api/', urls.include('rest_framework.urls', namespace='rest_framework')),
+    urls.url(r'^schema/', schemas.get_schema_view(title='Pastebin API')),
+    urls.url(r'^docs/', documentation.include_docs_urls(title='Documentação', description='Live API')),
     urls.url(r'^admin/', admin.site.urls),
 ]
 
